665ec252d60fa2ce2e1b24ae517b4f1a5f9f8694
Marble <mrmelonman1222@gmail.com>
2025-10-20 05:46:33 +0900

    Chenged pins

--- follower/pins.h
+++ follower/pins.h
@@ -2,18 +2,18 @@
 
 #include <Arduino.h>
 
-constexpr uint8_t PIN_TRIG_LEFT  = 8;
-constexpr uint8_t PIN_ECHO_LEFT  = 9;
+constexpr uint8_t PIN_IR_OBST    = 2;
 
-constexpr uint8_t PIN_TRIG_RIGHT = 10;
-constexpr uint8_t PIN_ECHO_RIGHT = 11;
+constexpr uint8_t PIN_LEFT_PWM   = 3;
+constexpr uint8_t PIN_LEFT_IN1   = 4;
+constexpr uint8_t PIN_LEFT_IN2   = 5;
 
-constexpr uint8_t PIN_IR_OBST    = 7;
-
-constexpr uint8_t PIN_LEFT_IN1   = 2;
-constexpr uint8_t PIN_LEFT_IN2   = 3;
-constexpr uint8_t PIN_LEFT_PWM   = 5;
-
-constexpr uint8_t PIN_RIGHT_IN1  = 4;
-constexpr uint8_t PIN_RIGHT_IN2  = 12;
 constexpr uint8_t PIN_RIGHT_PWM  = 6;
+constexpr uint8_t PIN_RIGHT_IN1  = 7;
+constexpr uint8_t PIN_RIGHT_IN2  = 8;
+
+constexpr uint8_t PIN_TRIG_LEFT  = 9;
+constexpr uint8_t PIN_ECHO_LEFT  = 10;
+
+constexpr uint8_t PIN_TRIG_RIGHT = 11;
+constexpr uint8_t PIN_ECHO_RIGHT = 12;
